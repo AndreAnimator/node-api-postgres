23204e762fad77dc19daa1522e68878f90933783
André Vitor da Silva Cardoso <98904347+AndreAnimator@users.noreply.github.com>
2026-06-28 03:14:51 -0300

    Created simple Crud for users

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors'
+import * as db from './querie.js'
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+)
+app.use(cors())
+
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
